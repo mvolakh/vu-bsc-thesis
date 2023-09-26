394786f394fc7d67987c3ad9fc0426b16a0ac784
Mikita Volakh <m.volakh@gmail.com>
2023-09-26 21:15:12 +0200

    feat: add mqtt handler

---
+++ server/mqtt/mqttHandler.js
@@ -0,0 +1,30 @@
+const mqtt = require('mqtt');
+const colors = require('colors');
+
+const mqttOptions = {
+    clientId: `mqtt_${Math.random().toString(16).slice(3)}`,
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD
+};
+
+const connect =  async () => {
+    const mqttClient = mqtt.connect(`mqtt://${process.env.MQTT_HOST}`, mqttOptions)
+
+    mqttClient.on('connect', () => {
+        console.log(`[MQTT] ${colors.green("Connection established successfully")}`);
+    
+        mqttClient.subscribe([process.env.MQTT_TOPIC], () => {
+            console.log(`[MQTT] ${colors.green("Subscribed to topic:")} ${colors.blue(process.env.MQTT_TOPIC)}`);
+        })
+    });
+
+    mqttClient.on('message', async (topic, payload) => {
+        console.log(`[MQTT] ${colors.green("Received message:")} ${colors.blue(topic)} ${payload.toString()}`);
+
+        const jsonData = JSON.parse(payload);
+    });
+
+    return mqttClient;
+}
+
+module.exports = { connect };
