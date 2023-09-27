d5693541decfaf864036337e485893c036cf3a06
Mikita Volakh <m.volakh@gmail.com>
2023-09-27 07:16:02 +0200

    feat: initialize a vue app

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent, ref, onMounted, onBeforeUnmount } from 'vue';
+import {io, Socket} from 'socket.io-client';
+
+export default defineComponent({
+    name: 'Home',
+    components: { },
+    setup() {
+        const test = ref()
+        let socket = ref<Socket | null>();
+
+        async function ioConnect() {
+            socket.value = io('http://localhost:8000');
+        }
+
+        onMounted(() => {
+            ioConnect();
+        });
+
+        onBeforeUnmount(() => {
+            socket.value?.disconnect();
+        });
+
+        return { test, ioConnect }
+    }
+})
+</script>
+
+<template>
+    <h1>Hello World</h1>
+    <!-- <button @click="ioConnect()"></button> -->
+</template>
+
+<style scoped>
+</style>
