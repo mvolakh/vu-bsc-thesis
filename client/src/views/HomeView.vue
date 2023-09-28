d219dc79e445602d65bb686aca82aef4076135b5
Mikita Volakh <m.volakh@gmail.com>
2023-09-28 03:33:45 +0200

    feat: update component template

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,37 +2,41 @@
 import { defineComponent, ref, onMounted, onBeforeUnmount } from 'vue';
 import {io, Socket} from 'socket.io-client';
 
+
 export default defineComponent({
     name: 'Home',
-    components: { },
+    components: {},
     setup() {
         const test = ref()
-        let socket = ref<Socket | null>();
+        // let socket = ref<Socket | null>();
 
-        async function ioConnect() {
-            socket.value = io('http://localhost:8000');
+        // async function ioConnect() {
+        //     socket.value = io('http://localhost:8000');
 
-            socket.value.on("mqttData", (mqttData) => {
-                console.log("Received MQTT data:", mqttData)
-            })
-        }
+        //     socket.value.on("mqttData", (mqttData) => {
+        //         console.log("Received MQTT data:", mqttData)
+        //     })
+        // }
 
-        onMounted(() => {
-            ioConnect();
-        });
+        // onMounted(() => {
+        //     ioConnect();
+        // });
 
-        onBeforeUnmount(() => {
-            socket.value?.disconnect();
-        });
+        // onBeforeUnmount(() => {
+        //     socket.value?.disconnect();
+        // });
 
-        return { test, ioConnect }
+        return { test }
     }
 })
 </script>
 
 <template>
-    <h1>Hello World</h1>
-    <!-- <button @click="ioConnect()"></button> -->
+    <v-img
+        fill-height
+        cover
+        src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+    </v-img>
 </template>
 
 <style scoped>
