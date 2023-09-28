826d3e8480520504ebb4caf165c622717110ffc3
Mikita Volakh <m.volakh@gmail.com>
2023-09-28 03:32:02 +0200

    chore: install vuetify lib

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,8 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import "@mdi/font/css/materialdesignicons.css";
 
-app.use(router)
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    }
+})
 
-app.mount('#app')
+createApp(App).use(vuetify).use(router).mount('#app')
