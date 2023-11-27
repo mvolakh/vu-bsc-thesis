c8fe97c452788692f0aeed2d414e6f09a67b659c
Mikita Volakh <m.volakh@gmail.com>
2023-11-27 13:52:34 +0100

    feat: register a new route for predictions

--- server/app.js
+++ server/app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const colors = require('colors');
 const morgan = require('morgan');
 const dotenv = require('dotenv').config();
+const dotenvExpand = require("dotenv-expand").expand(dotenv);
 const cors = require('cors');
 
 const mqttHandler = require('./mqtt/mqttHandler');
 const dbHandler = require('./db/dbHandler');
 const socketHandler = require('./socketio/socketHandler');
+const scheduleHandler = require('./scheduler/scheduleHandler');
 
 const sensorDataRouter = require('./routes/sensorData')
 const roomDataRouter = require('./routes/roomData')
+const forecastRouter = require('./routes/forecast')
 
 const app = express();
 
@@ -25,10 +28,12 @@ const initializeApp = async () => {
         await dbHandler.connect();
         const io = await socketHandler.connect(server);
         const mqttClient = await mqttHandler.connect(io);
+        const scheduler = await scheduleHandler.schedulePredictions(io);
 
 
         app.use('/api/sensordata', sensorDataRouter);
         app.use('/api/roomdata', roomDataRouter);
+        app.use('/api/forecast', forecastRouter);
     } catch (err) {
         console.error(`[APP] ${colors.red(`Error initializing the application: ${err}`)}`);
     }
